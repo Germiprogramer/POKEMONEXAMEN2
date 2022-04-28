5c96e2a854a1273bd39af9b3f8f939992f047334
Germiprogramer <llorentemunozgerman@gmail.com>
2022-04-28 10:25:28 +0200

    explicacion final

--- ordenarpokemonporpoder.py
+++ ordenarpokemonporpoder.py
@@ -5,3 +5,5 @@ health_points.sort()
 from random import randint
 
 pokemon = health_points[randint(1,100)]
+
+#no me ha dado timepo a acabar esto, pero era un plan para obtener los pokemon mas fuertes para que luchem entre ellos
