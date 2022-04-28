9736422dfb39655dfb3e710ea7738d31e203f02a
Germiprogramer <llorentemunozgerman@gmail.com>
2022-04-28 09:23:04 +0200

    moda

--- analisis.py
+++ analisis.py
@@ -128,4 +128,78 @@ print("La mediana de las notas de lectura es: {}".format(mediana_attack))
 mediana_defense = (defense_rating[valor_medio] + defense_rating[valor_medio + 1])/2
 print("La mediana de las notas de escritura  es: {}".format(mediana_defense))
 
-#
+# 5.MODA
+print("- MODA -")
+
+diccionario_conteo_health = {}
+for numero in health_points:
+    clave = str(numero)
+   
+    if not clave in diccionario_conteo_health:
+        
+        diccionario_conteo_health[clave] = 1
+    
+    else:
+        
+        diccionario_conteo_health[clave] += 1
+
+frecuencia_mayor = 0
+numero_mas_repetido = health_points[0]
+
+for numero in diccionario_conteo_health:
+    if diccionario_conteo_health[numero] > frecuencia_mayor:
+        numero_mas_repetido = numero
+        frecuencia_mayor = diccionario_conteo_health[numero]
+
+conteo = diccionario_conteo_health[str(numero_mas_repetido)]
+print(f"La nota de mates más repetida es {numero_mas_repetido} (encontrado {conteo} ocasiones)")
+
+
+
+diccionario_conteo_attack = {}
+for numero in attack_rating:
+    clave = str(numero)
+   
+    if not clave in diccionario_conteo_attack:
+        
+        diccionario_conteo_attack[clave] = 1
+    
+    else:
+        
+        diccionario_conteo_attack[clave] += 1
+
+frecuencia_mayor = 0
+numero_mas_repetido = attack_rating[0]
+
+for numero in diccionario_conteo_attack:
+    if diccionario_conteo_attack[numero] > frecuencia_mayor:
+        numero_mas_repetido = numero
+        frecuencia_mayor = diccionario_conteo_attack[numero]
+
+conteo = diccionario_conteo_attack[str(numero_mas_repetido)]
+print(f"La nota de lectura más repetida es {numero_mas_repetido} (encontrado {conteo} ocasiones)")
+
+
+
+diccionario_conteo_escritura = {}
+for numero in defense_rating:
+    clave = str(numero)
+   
+    if not clave in diccionario_conteo_escritura:
+        
+        diccionario_conteo_escritura[clave] = 1
+    
+    else:
+        
+        diccionario_conteo_escritura[clave] += 1
+
+frecuencia_mayor = 0
+numero_mas_repetido = defense_rating[0]
+
+for numero in diccionario_conteo_escritura:
+    if diccionario_conteo_escritura[numero] > frecuencia_mayor:
+        numero_mas_repetido = numero
+        frecuencia_mayor = diccionario_conteo_escritura[numero]
+
+conteo = diccionario_conteo_escritura[str(numero_mas_repetido)]
+print(f"La nota de escritura más repetida es {numero_mas_repetido} (encontrado {conteo} ocasiones)")
